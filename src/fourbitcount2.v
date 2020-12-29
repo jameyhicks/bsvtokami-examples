4fbad175eb6e80a3f4f7e8e8ebcbeb13b998e768
Jamey Hicks <jamey.hicks@accelerated.tech>
2020-12-29 16:35:01 -0500

    revised impl and specs and added some proofs

--- src/fourbitcount2.v
+++ src/fourbitcount2.v
@@ -16,55 +16,180 @@ Module module'mkTb.
   Section section'mkTb.
     Variable instancePrefix : string.
     Definition mkTb := (MODULE {
-    Register "c" : (Bit 4) <- Default
+    Register (instancePrefix--"c") : (Bit 4) <- Default
     with 
-    Rule (instancePrefix--"increment") := 
+    Rule ("increment") := 
     (
-        Read c_val : (Bit 4) <- "c" ;
-        Assert(#c_val <= $15) ;
-        Write "c" : Bit 4 <- #c_val + $1 ;
-        Call unused : Void <- (MethodSig ("$display") ((Bit 4)) : Void) (#c_val) ;
-
+        Read c : (Bit 4) <- (instancePrefix--"c") ;
+        Assert(#c < $15) ;
+        Write (instancePrefix--"c") : Bit 4 <- #c + $1 ;
+        Call unused : Void <- (MethodSig ("$display") ((Bit 4)) : Void) (#c) ;
     Retv
     )
 
     with 
-    Rule (instancePrefix--"increment1") := 
+    Rule ("done") := 
     (
-        Read c_val : (Bit 4) <- "c" ;
-        Assert(#c_val == $15) ;
-        Call unused : Void <- (MethodSig ("$display") ((Bit 4)) : Void) (#c_val) ;
-
-    Retv
-    )
-
-    with 
-    Rule (instancePrefix--"done") := 
-    (
-        Read c_val : (Bit 4) <- "c" ;
-        Assert(#c_val >= $15) ;
+        Read c : (Bit 4) <- (instancePrefix--"c") ;
+        Assert(#c == $15) ;
         Call unused : Void <- (MethodSig ("$finish") () : Void)  () ;
 
     Retv
     )
 
     }).
+
+    Definition mkTb1 := (MODULE {
+    Register (instancePrefix--"c") : (Bit 4) <- Default
+    with 
+    Rule ("increment") := 
+    (
+        Read c : (Bit 4) <- (instancePrefix--"c") ;
+        If (#c < $15) then (
+	    Write (instancePrefix--"c") : Bit 4 <- #c + $1 ;
+	    Call unused : Void <- (MethodSig ("$display") ((Bit 4)) : Void) (#c) ;
+            Retv ) else (Retv) ;
+         Retv
+    )
+
+    with 
+    Rule ("done") := 
+    (
+        Read c : (Bit 4) <- (instancePrefix--"c") ;
+        If (#c == $15)  then (Call unused : Void <- (MethodSig ("$finish") () : Void)  () ; Retv ) else (Retv) ;
+        Retv
+    )
+
+    }).
   End section'mkTb.
 End module'mkTb.
 
-Module spec. 
+Module module'spec.
   Section section'spec.
   Variable instancePrefix : string.
   Definition spec:=
   MODULE {
      Register (instancePrefix -- "c") : Bit 4 <- Default
      
-    with Rule "increment" :=
-       Read c : Bit 4 <- "c" ;
-       If (#c <= $15) then (Write "c" : Bit 4 <- #c + $1 ; Retv ) else ( Retv );
-       If (#c == $15) then (Call unused : Void <- (MethodSig ("$finish") () : Void)  () ; Retv ) else ( Retv );
-       If (#c >= $15) then (Call unused1 : Void <- (MethodSig ("display") ((Bit 4)):Void) (#c) ; Retv ) else (Retv);
+    with Rule "spec_rule" :=
+       Read c : Bit 4 <- (instancePrefix--"c") ;
+       If (#c < $15) then (Write (instancePrefix--"c") : Bit 4 <- #c + $1 ; Call unused1 : Void <- (MethodSig ("$display") ((Bit 4)):Void) (#c); Retv ) else ( Retv );
+       If (#c == $15) then (Call unused : Void <- (MethodSig ("$finish") () : Void)  () ; Retv ) else (Retv);
       Retv
     }.
+
+  Definition spec0 :=
+  MODULE {
+     Register (instancePrefix -- "c") : Bit 4 <- Default
+   with  
+    Rule ("increment") := 
+    (
+        Read c : (Bit 4) <- (instancePrefix--"c") ;
+        Assert(#c < $15) ;
+        Write (instancePrefix--"c") : Bit 4 <- #c + $1 ;
+        Call unused : Void <- (MethodSig ("$display") ((Bit 4)) : Void) (#c) ;
+    Retv
+    )
+    with 
+    Rule ("done") := 
+    (
+        Read c : (Bit 4) <- (instancePrefix--"c") ;
+        Assert(#c == $15) ;
+        Call unused : Void <- (MethodSig ("$finish") () : Void)  () ;
+        Retv
+    )
+   }.
+
+  Definition spec1 :=
+  MODULE {
+     Register (instancePrefix -- "c") : Bit 4 <- Default
+   with  
+    Rule ("increment") := 
+    (
+        Read c : (Bit 4) <- (instancePrefix--"c") ;
+        Assert(#c < $15) ;
+        Write (instancePrefix--"c") : Bit 4 <- #c + $1 ;
+        Call unused : Void <- (MethodSig ("$display") ((Bit 4)) : Void) (#c) ;
+    Retv
+    )
+    with 
+    Rule ("done") := 
+    (
+        Read c : (Bit 4) <- (instancePrefix--"c") ;
+        Assert(#c == $15) ;
+        Call unused : Void <- (MethodSig ("$finish") () : Void)  () ;
+
+    Retv
+    )
+    with 
+    Rule ("extra") := 
+    (
+        Read c : (Bit 4) <- (instancePrefix--"c") ;
+        Assert(#c == $7) ;
+        Call unused : Void <- (MethodSig ("extra") () : Void)  () ;
+
+    Retv
+    )
+   }.
+
+  Definition spec2 :=
+  MODULE {
+     Register (instancePrefix -- "c") : Bit 4 <- Default
+   with  
+    Rule ("increment") := 
+    (
+        Read c : (Bit 4) <- (instancePrefix--"c") ;
+        If (#c < $15) then (
+	    Write (instancePrefix--"c") : Bit 4 <- #c + $1 ;
+	    Call unused : Void <- (MethodSig ("$display") ((Bit 4)) : Void) (#c) ;
+            Retv ) else (Retv) ;
+         Retv
+    )
+
+    with 
+    Rule ("done") := 
+    (
+        Read c : (Bit 4) <- (instancePrefix--"c") ;
+        If (#c == $15)  then (Call unused : Void <- (MethodSig ("$finish") () : Void)  () ; Retv ) else (Retv) ;
+        Retv
+    )
+  }.
    End section'spec.
-End spec.
+End module'spec.
+
+Hint Unfold module'mkTb.mkTb : ModuleDefs.
+Hint Unfold module'mkTb.mkTb1 : ModuleDefs.
+
+Lemma impl_ModEquiv:
+  ModPhoasWf (module'mkTb.mkTb "top").
+Proof. kequiv. Qed.
+Hint Resolve impl_ModEquiv.
+
+Lemma impl1_ModEquiv:
+  ModPhoasWf (module'mkTb.mkTb1 "top").
+Proof. kequiv. Qed.
+Hint Resolve impl1_ModEquiv.
+
+Hint Unfold module'spec.spec : ModuleDefs.
+Lemma spec_ModEquiv:
+  ModPhoasWf (module'spec.spec "top").
+Proof. kequiv. Qed.
+Hint Resolve spec_ModEquiv.
+
+Hint Unfold module'spec.spec0 : ModuleDefs.
+Lemma spec0_ModEquiv:
+  ModPhoasWf (module'spec.spec0 "top").
+Proof. kequiv. Qed.
+Hint Resolve spec0_ModEquiv.
+
+Hint Unfold module'spec.spec1 : ModuleDefs.
+Lemma spec1_ModEquiv:
+  ModPhoasWf (module'spec.spec1 "top").
+Proof. kequiv. Qed.
+Hint Resolve spec1_ModEquiv.
+
+Hint Unfold module'spec.spec2 : ModuleDefs.
+Lemma spec2_ModEquiv:
+  ModPhoasWf (module'spec.spec2 "top").
+Proof. kequiv. Qed.
+Hint Resolve spec2_ModEquiv.
--- src/fourbitcounter_OK.v
+++ src/fourbitcounter_OK.v
@@ -11,10 +11,68 @@ Require Import fourbitcount2.
 
 Set Implicit Arguments.
 
+Definition ruleMap0 (_: RegsT): string -> option string :=
+  "increment" |-> "increment"; "done" |-> "done"; ||.
+Definition ruleMap1 (_: RegsT): string -> option string :=
+  "increment" |-> "increment"; "done" |-> "done"; ||.
+
+Definition regMap (r: RegsT): RegsT.
+  (* We are using tactics to build a map from register valuations in [impl] to register valuations in [spec]. *)
+  kgetv "c.top"%string datav r (Bit 4) (M.empty _: RegsT).
+  (* First, extract the value of [impl] register ["data"]. *)
+  exact (M.add "c.top"%string (existT _ _ datav) (M.empty _)).
+  (* Then, give the corresponding values of all registers for [spec]. *)
+Defined.
+Hint Unfold ruleMap0: MethDefs.
+Hint Unfold ruleMap1: MethDefs.
+Hint Unfold regMap: MethDefs. (* for kdecompose_regMap_init *)
+
+Theorem fourbitcounter_0_OK :
+   fourbitcount2.module'mkTb.mkTb "top" <<== fourbitcount2.module'spec.spec0 "top".
+Proof.
+    kinline_compute.
+    kdecompose_nodefs regMap ruleMap0.
+    kinvert.
+    kinv_magic.
+    * destruct wlt_dec. trivial. contradiction.
+    * kinv_magic.
+Qed.
+
+Theorem fourbitcounter_1_OK :
+   fourbitcount2.module'mkTb.mkTb "top" <<== fourbitcount2.module'spec.spec1 "top".
+Proof.
+    kinline_compute.
+    kdecompose_nodefs regMap ruleMap1.
+    kinvert.
+    kinv_magic.
+    * trivial.  kinv_magic.
+   destruct wlt_dec. trivial. contradiction.
+    * kinv_magic.
+Qed.
+
+Definition ruleMap (_: RegsT): string -> option string :=
+  "increment" |-> "spec_rule"; "done" |-> "spec_rule"; ||.
+Hint Unfold ruleMap: MethDefs.
+Hint Unfold regMap: MethDefs. (* for kdecompose_regMap_init *)
+
 Theorem fourbitcounter_OK :
-   fourbitcount2.spec.spec "top" <<== fourbitcount2.module'mkTb.mkTb "top".
+   fourbitcount2.module'mkTb.mkTb "top" <<== fourbitcount2.module'spec.spec "top".
 
 Proof.
+kinline_compute.
+kdecompose_nodefs regMap ruleMap.
+kinv_magic.
 admit. (* fixme: proof goes here *)
 
 Admitted.
+
+Theorem fourbitcounter_2_OK :
+   fourbitcount2.module'spec.spec2 "top" <<== fourbitcount2.module'spec.spec2 "top".
+
+Proof.
+kinline_compute.
+kdecompose_nodefs regMap ruleMap1.
+kinv_magic.
+admit. (* fixme proof *)
+Admitted.
+
